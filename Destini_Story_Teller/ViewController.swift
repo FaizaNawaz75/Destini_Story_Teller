91f04348b049fd10040d3301ba305b111150b754
Faiza Nawaz <Faizanawaz755@gmail.com>
2023-04-02 11:20:07 +0500

    Initial Commit

---
+++ Destini_Story_Teller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini_Story_Teller
+//
+//  Created by Faiza Nawaz on 02/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
